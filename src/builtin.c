c8d0262fb57b3d392af86494ccb303d33787801c
John Doe <johndoe@example.com>
2023-06-01 15:39:21 +0300

    everything

---
+++ src/builtin.c
@@ -0,0 +1,202 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atuliara <atuliara@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/24 12:50:33 by atuliara          #+#    #+#             */
+/*   Updated: 2023/06/01 13:06:14 by atuliara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+extern int g_exit_status;
+
+int execute_builtin_cd(t_list *list)
+{
+	char *path;
+	
+	path = list->command.full_cmd[1];
+    if (chdir(path) != 0)
+	{
+        error_handling("cd erore");
+    }
+    g_exit_status = 1337;
+	return (g_exit_status);
+}
+
+int execute_builtin_pwd()
+{
+    char cwd[4096];
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL) 
+    {
+        write(STDOUT_FILENO, cwd, ft_strlen(cwd));
+        write(STDOUT_FILENO, "\n", 1);
+    } 
+    else 
+		error_handling("pwd error");
+	g_exit_status = 1338; //?
+	return (g_exit_status);
+}
+
+int execute_builtin_exit()
+{
+	write(1, "exit\n", 5);
+	exit (0);
+}
+
+int execute_builtin_env(char **envp)
+{
+	char **tmp;
+
+	tmp = envp;
+	while (*tmp)
+	{
+		ft_putstr_fd(*tmp++, 1);
+		ft_putstr_fd("\n", 1);
+	}
+	return (0);
+}
+
+int is_in_env(char *str, char **envp)
+{
+	int len;
+	int count;
+	int i;
+
+	i = 0;
+	count = 0;
+	len = 0;
+	while (str[len] && str[len] != '=')
+		len++;
+	while (envp[i] && str[0] != 0)
+	{
+		if (!ft_strncmp(envp[i], str, len) && envp[i][len] == '=')
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+char **replace_str(char *str, char **envp)
+{
+	int len;
+	int count;
+	int i;
+
+	len = 0;
+	i = 0;
+	count = 0;
+	while (str[len] != '=')
+		len++;
+	while (envp[i] && str[0] != 0)
+	{
+		if (!ft_strncmp(envp[i], str, len) && envp[i][len] == '=')
+		{
+			free(envp[i]);
+			envp[i] = ft_strdup(str);
+		}
+		i++;
+	}
+	return (envp);
+}
+
+char **append_2d(char **twod, char *str_to_add)
+{
+	char **new;
+	int i;
+
+	i = 0;
+	while(twod[i])
+		i++;
+	new = (char **)malloc(sizeof(char *) * (i + 2));
+	i = -1;
+	while (twod[++i])
+		new[i] = ft_strdup(twod[i]);
+	new[i] = ft_strdup(str_to_add);
+	new[++i] = 0;
+	free_string_array(twod);
+	return (new);
+}
+
+int execute_builtin_export(t_list *list, t_resrc *resrc)
+{
+	int j;
+
+	j = 1;
+	while (list->command.full_cmd[j])
+	{
+		if (is_in_env(list->command.full_cmd[j], resrc->envp))
+			resrc->envp = replace_str(list->command.full_cmd[j], resrc->envp);
+		else if (ft_strchr(list->command.full_cmd[j], '=') != NULL)
+			resrc->envp = append_2d(resrc->envp, list->command.full_cmd[j]);
+		j++;
+	}
+	return (1);
+}
+
+char **rmv_str_twod(char **env, char *to_rmv)
+{
+	char **new;
+	int i;
+	int len;
+	int j;
+
+	i = 0;
+	j = 0;
+	len = ft_strlen(to_rmv);
+	while(env[i])
+		i++;
+	new = (char **)malloc(sizeof(char *) * \
+	(i + 1 - is_in_env(to_rmv, env)));
+	i = 0;
+	while (env[i])
+	{			
+		if (ft_strnstr(env[i], to_rmv, len))
+			i++;
+		new[j++] = ft_strdup(env[i++]);
+	}
+	new[j] = 0;
+	free_string_array(env);
+	return(new);
+}
+
+int execute_builtin_unset(t_list *list, t_resrc *resrc)
+{
+	int ac;
+
+	ac = 1;
+	while (list->command.full_cmd[ac] && is_in_env(list->command.full_cmd[ac], resrc->envp))
+	{
+		resrc->envp = rmv_str_twod(resrc->envp, list->command.full_cmd[ac]);
+		ac++;
+	}
+	return (1);
+}
+
+int execute_builtin_echo(t_command cmd)
+{
+    int newline;
+	int i;
+
+	newline = 1;
+	i = 1;
+    if (*cmd.full_cmd != 0 && cmd.full_cmd[i] != 0 \
+	&& ft_strncmp(cmd.full_cmd[i], "-n", 2) == 0)
+    {
+		newline = 0;
+		i++;
+	}
+    while (cmd.full_cmd[i] != 0)
+    {
+    	ft_putstr_fd(cmd.full_cmd[i], STDOUT_FILENO);
+        if (cmd.full_cmd[++i] != 0)
+            write(1, " ", 1);
+    }
+    if (newline)
+        ft_putstr_fd("\n", 1);
+    return (0);
+}
---
+++ src/exec.c
@@ -0,0 +1,189 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atuliara <atuliara@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 15:06:37 by atuliara          #+#    #+#             */
+/*   Updated: 2023/06/01 15:34:18 by atuliara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+extern int g_exit_status;
+
+int check_input(char **cmd_arr)
+{
+	int i;
+
+	i = 0;
+	while (cmd_arr[i])
+	{
+		if (ft_strchr(cmd_arr[i], '=') != NULL)
+			{
+				write(1, "invalid identifier\n", 19);
+				return (0);
+			}
+		i++;
+	}
+	return (1);
+}
+
+int check_for_parent_builtin(t_resrc *resrc, char **cmd_arr, int len)
+{	
+	if (!ft_strncmp(*cmd_arr, "cd", len))
+ 	   	return (execute_builtin_cd(resrc->list));
+	else if (!ft_strncmp(*cmd_arr, "unset", len) && check_input(cmd_arr))
+        return (execute_builtin_unset(resrc->list, resrc));
+	else if (!ft_strncmp(*cmd_arr, "export", 6))
+        return (execute_builtin_export(resrc->list, resrc));
+	return (0);
+}
+
+void setup_pipe(int *fd)
+{
+	if (dup2(fd[1], STDOUT_FILENO) < 0)
+		error_handling("pipe error");
+	close(fd[1]);
+	close(fd[0]);
+}
+
+void close_pipes(t_list *list, int *fd)
+{
+	close(fd[0]);
+	if (list->command.input_fd > 2)
+		close(list->command.input_fd);
+	if (list->command.output_fd > 2)
+		close(list->command.output_fd);
+}
+
+void execute_builtin(t_resrc *resrc, t_list *list) 
+{
+	char *cmd;
+	int len;
+	
+	len = 0;
+	cmd = *list->command.full_cmd;
+	if (*list->command.full_cmd)
+		len = ft_strlen(cmd);
+    if (!ft_strncmp(cmd, "pwd", len))
+    	g_exit_status = execute_builtin_pwd();
+ 	else if (!ft_strncmp(cmd, "echo", len))
+    	g_exit_status = execute_builtin_echo(list->command);
+	else if (!ft_strncmp(cmd, "env", len))
+    	g_exit_status = execute_builtin_env(resrc->envp);
+}
+
+void execute_child(t_resrc *resrc, t_list *list)
+{
+	// signals
+	if (list->command.full_path)
+		execve(list->command.full_path, list->command.full_cmd, resrc->envp);
+	else if (is_builtin(*list->command.full_cmd))
+		execute_builtin(resrc, list);
+	else
+		execve(*list->command.full_cmd, NULL, resrc->envp);
+	exit(g_exit_status);
+}
+
+int setup_redir(t_list *list)
+{
+    if (list->command.input_fd != STDIN_FILENO) 
+	{
+		dup2(list->command.input_fd, STDIN_FILENO); //handle error later
+		close(list->command.input_fd);
+    }
+	if (list->command.output_fd != STDOUT_FILENO) 
+	{
+        dup2(list->command.output_fd, STDOUT_FILENO); // handle error later
+		close(list->command.output_fd);
+    }
+    return 0;
+}
+
+void child_process(t_resrc *resrc, t_list *list, int *fd)
+{
+	setup_redir(list);
+	if (list->next)
+		setup_pipe(fd);
+	execute_child(resrc, list);
+}
+
+void do_fork(t_resrc *resrc, t_list *list, int *fd)
+{
+	pid_t pid;
+	
+	pid = fork();
+	if (pid < 0)
+		error_handling("fork error");
+	if (!pid)
+		child_process(resrc, list, fd);
+}
+
+void exec_cmd(t_resrc *resrc, t_list *list)
+{
+	int fd[2];
+
+	if (pipe(fd) < 0)
+	{
+		close_pipes(list, fd);
+		error_handling("pipe error");
+	}
+	do_fork(resrc, list, fd);
+	close(fd[1]);
+	if (list->next && list->next->command.input_fd == 0)
+		list->next->command.input_fd = fd[0];
+	else
+		close_pipes(list, fd);
+}
+
+int cmd_check(t_list *list)
+{
+	if (is_a_directory(*list->command.full_cmd))
+	{
+		print_error(": is a directory\n", 126, *list->command.full_cmd);
+		return (0);
+	}
+	if (!is_builtin(*list->command.full_cmd) && !list->command.full_path &&\
+	access(*list->command.full_cmd, F_OK) == -1)
+	{
+		if (ft_strchr(*list->command.full_cmd, '/'))
+			print_error(": no such file or directory\n", 127, *list->command.full_cmd);
+		else
+			print_error(": command not found\n", 127, *list->command.full_cmd);
+		return (0);
+	}
+	else if (!is_builtin(*list->command.full_cmd) && !list->command.full_path &&\
+	access(*list->command.full_cmd, X_OK) == -1)
+	{
+		print_error(": permission denied\n", 127, *list->command.full_cmd);
+		return (0);
+	}
+	return (1);
+}
+
+void execution(t_resrc *resrc, t_list *list)
+{	
+	char **cmd_arr;
+	int len;
+	int lst_size; 
+
+	lst_size = linked_list_count(&list);
+	while (list)
+	{
+		cmd_arr = list->command.full_cmd;
+		if (cmd_arr)
+			len = ft_strlen(*cmd_arr);
+		if (!ft_strncmp(*cmd_arr, "exit", len))
+      	 	execute_builtin_exit();
+		if (!list->next)
+			g_exit_status = check_for_parent_builtin(resrc, cmd_arr, len);
+		//signals
+		if (cmd_check(list))
+			exec_cmd(resrc, list);
+		list = list->next;
+		wait_for_child(lst_size);
+	}
+}	
